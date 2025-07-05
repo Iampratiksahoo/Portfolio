8f88005164918c2bb16157d891685faabd50a4ce
Pratik Sahoo <iampratiksahoo@gmail.com>
2025-07-05 08:59:48 +0530

    Migrate site to Eleventy static generator

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function(eleventyConfig) {
+  // Copy static assets to the output folder
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  return {
+    dir: {
+      input: "src",
+      includes: "_includes",
+      layouts: "_layouts",
+      output: "_site"
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk"
+  };
+};
+
