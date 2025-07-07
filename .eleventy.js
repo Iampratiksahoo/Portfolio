6704c9cf71f8b4581790f3c4fad13db7fcedd622
Pratik Sahoo <iampratiksahoo@gmail.com>
2025-07-08 00:31:06 +0530

    fixed the js file copying in eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
   // Copy static assets to the output folder
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/CNAME");
-  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/assets/js");
 
   // Add endsWith filter
   eleventyConfig.addFilter("endsWith", function(str, suffix) {
